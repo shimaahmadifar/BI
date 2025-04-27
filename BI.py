298d6abc564bac242a845999b0c929f3e6c504e6
shima ahmadifar <116803314+shimaahmadifar@users.noreply.github.com>
2025-04-27 23:39:16 +0200

    Add files via upload

---
+++ BI.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Home",
+        options=["Data Preprocessing", "Data Analyse", "Modeling"],
+
+    )
+if selected == "Home":
+    st.title(f"You have selected {selected}")
+if selected == "Data Preprocessing":
+    st.title(f"You have selected {selected}")
+if selected == "Data Analyse":
+    st.title(f"You have selected {selected}")
+if selected == "Modeling":
+    st.title(f"You have selected {selected}")
