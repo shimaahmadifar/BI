9295ed4c0bf03f081d1cb07c016ff807b8197c60
shima ahmadifar <116803314+shimaahmadifar@users.noreply.github.com>
2025-04-28 10:41:12 +0200

    Update BI.py

--- BI.py
+++ BI.py
@@ -4,11 +4,17 @@ from streamlit_option_menu import option_menu
 with st.sidebar:
     selected = option_menu(
         menu_title="Home",
-        options=["Data Preprocessing", "Data Analyse", "Modeling"],
+        options=["Home","Data Preprocessing", "Data Analyse", "Modeling"],
 
     )
 if selected == "Home":
-    st.title(f"You have selected {selected}")
+    st.title(f"Information Systems & Business Intelligence")
+    st.title(f"Professor FLORA AMATO")
+    st.write("Insects Analysis and Prediction Using Weather Data and Time series")
+    st.write("\n"
+             "Seyed Sadeqh Elmi Mousavi-D030000?" "\n" "Negar Jafar Nejad-D030000?" "\n" "Shima Ahmadifar-D030000?" "\n"
+
+
 if selected == "Data Preprocessing":
     st.title(f"You have selected {selected}")
 if selected == "Data Analyse":
